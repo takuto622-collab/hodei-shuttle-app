620c1bd469cd43527e5278b45b65a5e0771fd2f6
takuto622-collab <takuto.622@gmail.com>
2025-09-04 00:02:37 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -659,7 +659,7 @@ export default function App() {
         {/* 右：8車グリッド */}
         <div className="p-3 h-full flex-1 min-w-0">
           <div className="grid grid-cols-4 grid-rows-2 gap-3 h-full">
-            {VEHICLE_IDS.map((vid, idx) => (
+            {VISIBLE_VEHICLES.map((vid, idx) => (
               <div
                 key={vid}
                 ref={vehicleRefs.current[vid]}
